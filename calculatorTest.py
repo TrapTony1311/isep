a09eac442d85ed8f60a89ec03d9e0e8d4a52e8ef
Anthony Nora <anthony@mimoli.com>
2025-04-16 12:02:32 +0200

    changed tests for parity with the java tests file

--- calculatorTest.py
+++ calculatorTest.py
@@ -18,18 +18,13 @@ class CalculatorTest:
         self.single_integer()
         self.single_float()
         self.addition()
-        self.addition_floats()
+        self.multiple_additions()
         self.subtraction()
-        self.subtraction_negative_result()
         self.multiplication()
-        self.multiplication_floats()
         self.multiple_operations()
-        self.operation_precedence()
-        self.parenthesus()
-        self.nested_parenthesis()
+        self.parenthesis()
         self.negatives()
-        self.negatives_parentheses()
-        self.complex_expression()
+        self.multiple_operations()
         print(f"\ntests passed: {self.passed}")
         print(f"tests failed: {self.failed}")
 
@@ -50,49 +45,61 @@ class CalculatorTest:
             self.failed += 1
 
     def single_integer(self):
-        self.is_equal(self.calculator.calculate("42"), 42, "Test for a solo integer")
+        self.is_equal(self.calculator.calculate("6"), 6, "Test for a solo integer")
+        self.is_equal(self.calculator.calculate("0"), 0, "Test for a solo integer")
+        self.is_equal(self.calculator.calculate("548511"), 548511, "Test for a solo integer")
 
     def single_float(self):
+        self.is_close_enough(self.calculator.calculate("8.9"), 8.9, "Test for a single float")
+        self.is_close_enough(self.calculator.calculate("1.0"), 1, "Test for a single float")
         self.is_close_enough(self.calculator.calculate("3.14"), 3.14, "Test for a single float")
 
     def addition(self):
-        self.is_equal(self.calculator.calculate(" 2 + 3"), 5, "Test for a single addition")
+        self.is_equal(self.calculator.calculate("1 + 1"), 2, "Test for a single addition")
+        self.is_equal(self.calculator.calculate("12 + 12"), 24, "Test for a single addition")
+        self.is_equal(self.calculator.calculate("13+12"), 25, "Test for a single addition")
+        self.is_equal(self.calculator.calculate("2+ 3"), 5, "Test for a single addition")
+        self.is_equal(self.calculator.calculate("8 +3"), 11, "Test for a single addition")
 
-    def addition_floats(self):
-        self.is_close_enough(self.calculator.calculate("2.5 + 3.1"), 5.6, "Test for a single addition with floats")
+    def multiple_additions(self):
+        self.is_equal(self.calculator.calculate("1 + 1 + 1"), 3, "Test for a multiple additions")
+        self.is_equal(self.calculator.calculate("12 + 12 + 1"), 25, "Test for a multiple additions")
+        self.is_equal(self.calculator.calculate("13+12+11"), 36, "Test for a multiple additions")
+        self.is_equal(self.calculator.calculate("2+ 3+ 5"), 10, "Test for a multiple additions")
+        self.is_equal(self.calculator.calculate("8 +3+3"), 14, "Test for a multiple additions")
 
     def subtraction(self):
-        self.is_equal(self.calculator.calculate("7 - 4"), 3, "Test for a simple substraction")
-
-    def subtraction_negative_result(self):
-        self.is_equal(self.calculator.calculate("2 - 5"), -3, "Test for a substraction that has a negative result")
+        self.is_equal(self.calculator.calculate("1 - 1"), 0, "Test for a simple substraction")
+        self.is_equal(self.calculator.calculate("8-5"), 3, "Test for a simple substraction")
+        self.is_equal(self.calculator.calculate("13-14"), -1, "Test for a simple substraction")
+        self.is_equal(self.calculator.calculate("20-10-5"), 5, "Test for a simple substraction")
+        self.is_equal(self.calculator.calculate("5 - 0 - 4 - 0 - 3"), -2, "Test for a simple substraction")
 
     def multiplication(self):
-        self.is_equal(self.calculator.calculate("3 * 4"), 12, "Testing with just a simple multiplication")
-
-    def multiplication_floats(self):
-        self.is_close_enough(self.calculator.calculate("2.5 * 4"), 10.0, "Test for a multiplication with a float in it")
-
-    def multiple_operations(self):
-        self.is_equal(self.calculator.calculate("2 + 3 * 4"), 14, "Test for an addition before a multiplication for operation precedence")
-
-    def operation_precedence(self):
-        self.is_equal(self.calculator.calculate("2 + 3 * 4 - 5"), 9, "More tests for operation precedence with different operations")
+        self.is_equal(self.calculator.calculate("0 * 0"), 0, "Testing with just a simple multiplication")
+        self.is_equal(self.calculator.calculate("0 * 1"), 0, "Testing with just a simple multiplication")
+        self.is_equal(self.calculator.calculate("1 * 0"), 0, "Testing with just a simple multiplication")
+        self.is_equal(self.calculator.calculate("2 * 2"), 4, "Testing with just a simple multiplication")
+        self.is_equal(self.calculator.calculate("1*2*3*4"), 24, "Testing with just a simple multiplication")
+        self.is_equal(self.calculator.calculate("9 * 0.5"), 4.5, "Testing with just a simple multiplication")
 
     def parenthesis(self):
-        self.is_equal(self.calculator.calculate("(2 + 3) * 4 "), 20, "Testing for parenthesis and precedence using a multiplication")
-
-    def nested_paretnhesis(self):
-        self.is_equal(self.calculator.calculate("2 * (3 + (4 - 1)) "), 12, "Testnig parenthesis in parenthesis")
+        self.is_equal(self.calculator.calculate("0 * (0 + 1)"), 0, "Testing for parenthesis")
+        self.is_equal(self.calculator.calculate("(1 + 2) * 2 + 1 "), 7, "Testing for parenthesis")
+        self.is_equal(self.calculator.calculate("(1 + 2) * (2 + 1) "), 9, "Testing for parenthesis")
+        self.is_equal(self.calculator.calculate("(5 - 5) * 0 - 4 "), -4, "Testing for parenthesis")
+        self.is_equal(self.calculator.calculate("(10 - 2) * 10"), 80, "Testing for parenthesis")
 
     def negatives(self):
-        self.is_equal(self.calculator.calculate("-3 + 7"), 4, "Test for a negative number in an addition")
+        self.is_equal(self.calculator.calculate("-15"), -15, "Test for a single negative number")
+        self.is_equal(self.calculator.calculate("-1"), -1, "Test for a single negative number")
 
-    def negatives_parentheses(self):
-        self.is_equal(self.calculator.calculate("2 * (-3 + 5)"), 4, "Test for a negative number inside a paretnhesis")
 
-    def complex_expression(self):
-        self.is_close_enough(self.calculator.calculate("3.5 + 2 * (4 - 1.5)"), 8.5, "Testing multiple cases in one expression")
+    def multiple_operations(self):
+        self.is_close_enough(self.calculator.calculate("0 * 0 + 1"), 1, "Testing multiple cases in one expression")
+        self.is_close_enough(self.calculator.calculate("1 + 2 * 2 + 1"), 6, "Testing multiple cases in one expression")
+        self.is_close_enough(self.calculator.calculate("5 - 5 * 0 - 4"), 1, "Testing multiple cases in one expression")
+        self.is_close_enough(self.calculator.calculate("10 - 2 * 10"), -10, "Testing multiple cases in one expression")
 
 # Run the tests
 
